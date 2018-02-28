fd6084140937d4855a332399e63976afe87e6e74
DJ <Djessa@live.de>
2018-02-28 14:57:49 +0100

    update gulpfile added lib folder

--- gulpfile.js
+++ gulpfile.js
@@ -7,24 +7,21 @@ var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
 var cleancss = require('gulp-clean-css');
 var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
 var gutil = require('gulp-util');
 var notify = require('gulp-notify');
 var rename = require('gulp-rename');
 var run = require('gulp-run');
-
-var jsImport = require('gulp-js-import');
 var babel = require('gulp-babel');
 var del = require('del');
 var uglify = require('gulp-uglify');
 
 var paths = {
   scripts: {
-    src: 'src/js/*.js',
+    src: 'src/js/**/*.js',
     dest: './assets/'
   }
 };
-
-// Include paths file.
 
 function swallowError(error) {
   // If you want details of the error in the console
@@ -36,37 +33,20 @@ var clean = function() {
   del(['dist']);
 };
 
-function scripts() {
-  return gulp.src(paths.scripts.src, {
-      sourcemaps: true
-    })
-    .pipe(babel())
-    .pipe(uglify())
-    .pipe(concat('main.min.js'))
-    .pipe(gulp.dest(paths.scripts.dest));
-}
-
-gulp.task('import', function() {
-  return gulp.src('./src/js/main.js')
-    .pipe(jsImport({
-      hideConsole: true
-    }))
-    .pipe(gulp.dest('./src/js/main.js'));
-});
-
 gulp.task('scripts', function() {
-  return gulp.src(paths.scripts.src, {
-      sourcemaps: true
-    })
+  return gulp.src(paths.scripts.src)
+    .pipe(sourcemaps.init())
     .pipe(babel())
     .pipe(uglify())
     .pipe(concat('main.min.js'))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(paths.scripts.dest));
 });
+
 
 gulp.task('watch', function() {
   gulp.watch('./src/sass_framework/**/*.scss', ['default']);
-  gulp.watch('./src/js/*.js', ['default']);
+  gulp.watch('./src/js/**/*.js', ['default']);
 });
 
 
@@ -91,7 +71,7 @@ gulp.task('sass', function() {
 });
 
 
-gulp.task('serve', ['sass', 'import', 'scripts'], function() {
+gulp.task('serve', ['sass', 'scripts'], function() {
 
 });
 
